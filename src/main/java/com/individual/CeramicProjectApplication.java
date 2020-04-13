008b6da2872d6b8052e760c680d4e9d860db395e
buivanhuan <chinhlaanh20019718@gmail.com>
2020-04-13 11:38:44 +0700

    new cm

--- src/main/java/com/individual/CeramicProjectApplication.java
+++ src/main/java/com/individual/CeramicProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoThemleafApplication {
+public class CeramicProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoThemleafApplication.class, args);
+		SpringApplication.run(CeramicProjectApplication.class, args);
 	}
 
 }
